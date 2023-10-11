9d960f5abdb0d1dc1e322c9cec06c4a2d6c08d22
whiHak <betse1111111111@gmail.com>
2023-10-11 22:04:48 +0300

    added Chats rendering futures

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import profile from '../images/profile.jpg'
+import React, { useContext, useEffect, useState } from "react";
+import profile from "../images/profile.jpg";
+import { AuthContext } from "../context/AuthContext";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+
 export const Chats = () => {
+  const currentUser = useContext(AuthContext);
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    const getChat = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+
+        return () => {
+          unsub();
+        };
+      });
+    };
+    currentUser.uid && getChat();
+  }, [currentUser.uid]);
+
+  console.log(Object.entries(chats));
   return (
-    <div className='chats'>
-      <div className='userInfo'>
-        <img src={profile} alt=''/>
-        <div className='chatsInfo'>
-          <span>Betse 95</span>
-          <p>blablab</p>
-        </div>
-      </div>
-      <div className='userInfo'>
-        <img src={profile} alt=''/>
-        <div className='chatsInfo'>
-          <span>Betse 95</span>
-          <p>blablab</p>
-        </div>
-      </div>
-      <div className='userInfo'>
-        <img src={profile} alt=''/>
-        <div className='chatsInfo'>
-          <span>Betse 95</span>
-          <p>blablab</p>
-        </div>
-      </div>
+    <div className="chats">
+      {Object.entries(chats)?.map((chats) => {
+        return (
+          <div className="userInfo" key={chats[0]}>
+            <img src={chats[1].userInfo.photoURL} alt="" />
+            <div className="chatsInfo">
+              <span>{chats[1].userInfo.name}</span>
+              <p>{chats[1].userInfo.lastMessages?.text}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,29 +1,76 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import profile from "../images/profile.jpg";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 export const Search = () => {
   const [user, setUser] = useState(null);
   const [username, setUsername] = useState("");
   const [err, setErr] = useState(false);
-  
+
+  const currentUser = useContext(AuthContext);
+
   const handleSearch = async () => {
     const q = query(collection(db, "users"), where("name", "==", username));
-    
+
     try {
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
-        setUser(doc.data())
+        setUser(doc.data());
       });
     } catch (error) {
       setErr(true);
     }
   };
-  
+
   const handleKey = (e) => {
     e.code === "Enter" && handleSearch();
   };
+
+  const handelSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const respose = await getDoc(doc(db, "chats", combinedId));
+
+      if (!respose.exists()) {
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            name: user.name,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            name: currentUser.name,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (error) {}
+    setUser(null)
+    setUsername("")
+  };
+
   return (
     <div className="search">
       <div className="form">
@@ -34,15 +81,18 @@ export const Search = () => {
           onChange={(e) => {
             setUsername(e.target.value);
           }}
+          value={username}
         />
       </div>
       {err && <span>user not found</span>}
-      {user && <div className="userInfo">
-        <img src={user.photoURL} alt="" />
-        <div className="chatInfo">
-          <span>{user.name}</span>
+      {user && (
+        <div className="userInfo" onClick={handelSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="chatInfo">
+            <span>{user.name}</span>
+          </div>
         </div>
-      </div>}
+      )}
     </div>
   );
 };
